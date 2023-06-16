7ab676aca1a1db41947f90599293e66d47aa4f1c
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-16 05:55:10 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+	printf ("Size of a char: %zu byte(su)\n", sizeof(char));
+	printf ("Size of an int: %zu byte(su)\n", sizeof (int));
+	printf ("Size of a long int: %zu byte(su)\n", sizeof (long int));
+	printf ("size of a long long int: %zu byte(su)\n"' sizeof (long long int));
+	printf ("Size of a float: %zu byte(su)\n", sizeof (float));
+
+		return (0);
+}
+	
+
