97f2f8b4ec131fa31330ec886add06253a04e604
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-18 07:55:41 -0700

    task 0

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdbio.h>
+#include <time>
+#include <stdbio.h>
+
+/**
+ * main - Determines if the number is positive, nagetive or zero.
+ * 
+ * return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if  (n > 0)
+	{
+		printf("%d is positive/n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero/n", n);
+	}
+	else
+	{
+		printf("%d is negative/n", n);
+	}
+	return (0);
+}
+
+	
+
+
