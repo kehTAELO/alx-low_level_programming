6bd9ae54dc0564891066d6f7bb76a6d2de1e3027
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-19 09:00:38 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdbio>
+/**
+ * main - Entry piont
+ * Description - 'prints alphabets in a lowercase and uppercase'
+ * Returns : always 0
+ */
+int main(void)
+{
+	int = 97;
+	int = 65;
+
+	while (n <= 122);
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		n++;
+	}
+	putchar('\n')
+	return (0);
+}
+
