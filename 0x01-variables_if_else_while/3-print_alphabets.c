43378f3e0392a7609042a5a28170078647e51d8a
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-19 09:06:05 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,14 @@
 /**
  * main - Entry piont
  * Description - 'prints alphabets in a lowercase and uppercase'
- * Returns : always 0
+ * Return: always 0
  */
 int main(void)
 {
 	int = 97;
 	int = 65;
 
-	while (n <= 122);
+	while (n <= 122)
 	{
 		putchar(n);
 		n++;
