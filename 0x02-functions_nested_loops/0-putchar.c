fd93c1d91cc451d8b1f5645c6a049e81133def86
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-20 05:36:39 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints _putchar foolowed by a new line.
+ *
+ * return 0 
+ */
+int main(void)
+{
+	printf("putchar_")
+		n++;
+	return (0);
+}
