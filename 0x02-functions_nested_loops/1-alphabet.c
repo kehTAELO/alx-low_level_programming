0519cbb2fb472df4d6ae2976b69da32abe545bdc
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-21 02:58:30 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - Make the alphabet.
+ * return: void.
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++);
+	{
+	putchar(c);
+	}
+	_putchar('\n');
+}
