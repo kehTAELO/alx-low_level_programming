02c4c6c07b7e699778796bf8c63159a5e50d83eb
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-21 00:50:41 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints the lowercase alphabet in reverse.
+ * return: 0
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 48; i < 58; i++);
+        {
+                putchar(i);
+        }
+        for (i = 97; i < 103; i++);
+        {
+                putchar(i);
+        }
+        putchar('\n');
+        return (0);
+~                                                                                                                                                                       
+~                           
