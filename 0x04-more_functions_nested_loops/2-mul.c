b8ca6b87339b08bc80b44a22c2b2dfcb65ff2a6b
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-22 00:48:14 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two characters
+ * @a: the first integer
+ * @d: the seconf integer
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
+
