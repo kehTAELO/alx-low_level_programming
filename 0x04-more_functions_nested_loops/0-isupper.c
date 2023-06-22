512875045d66d5105b9e403ac1e394926f5c2cec
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-22 00:34:23 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for uppercase character or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+return(1);
+}
+return (0);
+}
