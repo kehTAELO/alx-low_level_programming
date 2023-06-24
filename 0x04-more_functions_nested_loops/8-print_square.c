5f4bb1e9945199bad82859a4169748698b57bad0
kehTAELO <sylvestertaelo97@gmail.com>
2023-06-23 22:28:47 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,10 +17,10 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; < (size); j++)
+		for (j = 0; j < (size); j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
